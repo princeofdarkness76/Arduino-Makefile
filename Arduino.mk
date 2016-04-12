feae8b5888a36b38360f0425240c7585c5358b85
princeofdarkness76 <djsteviejames@hotmail.com>
2016-04-13 07:23:00 +0800

    Merge remote-tracking branch 'origin/auto-lib' into pod/arduino.mk
    # Conflicts:
    #	arduino-mk/Arduino.mk

--- Arduino.mk
+++ Arduino.mk
@@ -792,7 +792,7 @@ ifeq ($(strip $(CHK_SOURCES)),)
                 $(call show_config_info,No .pde or .ino files found. If you are compiling .c or .cpp files then you need to explicitly include Arduino header files)
             else
                 #TODO: Support more than one file. https://github.com/sudar/Arduino-Makefile/issues/49
-                $(error Need exactly one .pde or .ino file. This makefile doesn't support multiple .ino/.pde files yet)
+                $(error Need exactly one .pde or .ino file. This makefile doesn\'t support multiple .ino/.pde files yet)
             endif
         endif
 
@@ -822,6 +822,19 @@ endif
 
 
 ########################################################################
+# Automatically find the libraries needed to compile the sketch
+
+ifndef MAIN_LIBS
+    MAIN_LIBS = $(shell $(ARDMK_DIR)/bin/auto-lib.py $(USER_LIB_PATH) | \
+                sed -ne 's/MAIN_LIBS \(.*\) /\1/p')
+endif
+
+ifndef LIBS_DEPS
+    LIBS_DEPS = $(shell $(ARDMK_DIR)/bin/auto-lib.py $(USER_LIB_PATH) | \
+                sed -ne 's/LIBS_DEPS \(.*\) /\1/p')
+endif
+
+########################################################################
 # Determine ARDUINO_LIBS automatically
 
 ifndef ARDUINO_LIBS
@@ -829,11 +842,16 @@ ifndef ARDUINO_LIBS
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(ARDUINO_DIR)/libraries/*)), \
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(ARDUINO_SKETCHBOOK)/libraries/*)), \
+<<<<<<< HEAD
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(USER_LIB_PATH)/*)), \
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(ARDUINO_PLATFORM_LIB_PATH)/*)), \
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
+=======
+        $(shell sed -ne "s/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p" $(LOCAL_SRCS)))
+    ARDUINO_LIBS += $(MAIN_LIBS) $(LIBS_DEPS)
+>>>>>>> origin/auto-lib
 endif
 
 ########################################################################
@@ -1107,13 +1125,27 @@ else
     $(call show_config_info,Size utility: Basic (not AVR-aware),[AUTODETECTED])
 endif
 
-ifneq (,$(strip $(ARDUINO_LIBS)))
+ifneq (,$(strip $(MAIN_LIBS)))
     $(call arduino_output,-)
-    $(call show_config_info,ARDUINO_LIBS =)
+    $(call show_config_info,MAIN_LIBS =)
 endif
 
-ifneq (,$(strip $(USER_LIB_NAMES)))
-    $(foreach lib,$(USER_LIB_NAMES),$(call show_config_info,  $(lib),[USER]))
+ifneq (,$(strip $(MAIN_LIBS)))
+    $(foreach lib,$(MAIN_LIBS),$(call show_config_info,  $(lib),[USER]))
+endif
+
+ifneq (,$(strip $(LIBS_DEPS)))
+    $(call arduino_output,-)
+    $(call show_config_info,LIBS_DEPS =)
+endif
+
+ifneq (,$(strip $(LIBS_DEPS)))
+    $(foreach lib,$(LIBS_DEPS),$(call show_config_info,  $(lib),[USER]))
+endif
+
+ifneq (,$(strip $(SYS_LIBS)))
+    $(call arduino_output,-)
+    $(call show_config_info,SYS_LIBS =)
 endif
 
 ifneq (,$(strip $(SYS_LIB_NAMES)))
