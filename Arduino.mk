03c604e9eefe5a0c2689201524656e579db038eb
princeofdarkness76 <djsteviejames@hotmail.com>
2016-04-13 07:23:35 +0800

    Merge branch 'headers' into pod/arduino.mk
    # Conflicts:
    #	Arduino.mk

--- Arduino.mk
+++ Arduino.mk
@@ -792,7 +792,11 @@ ifeq ($(strip $(CHK_SOURCES)),)
                 $(call show_config_info,No .pde or .ino files found. If you are compiling .c or .cpp files then you need to explicitly include Arduino header files)
             else
                 #TODO: Support more than one file. https://github.com/sudar/Arduino-Makefile/issues/49
+<<<<<<< HEAD
                 $(error Need exactly one .pde or .ino file. This makefile doesn\'t support multiple .ino/.pde files yet)
+=======
+                $(error Need exactly one .pde or .ino file. This makefile doesn't support multiple .ino/.pde files yet)
+>>>>>>> headers
             endif
         endif
 
@@ -822,6 +826,7 @@ endif
 
 
 ########################################################################
+<<<<<<< HEAD
 # Automatically find the libraries needed to compile the sketch
 
 ifndef MAIN_LIBS
@@ -835,6 +840,8 @@ ifndef LIBS_DEPS
 endif
 
 ########################################################################
+=======
+>>>>>>> headers
 # Determine ARDUINO_LIBS automatically
 
 ifndef ARDUINO_LIBS
@@ -843,15 +850,21 @@ ifndef ARDUINO_LIBS
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(ARDUINO_SKETCHBOOK)/libraries/*)), \
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> headers
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(USER_LIB_PATH)/*)), \
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(filter $(notdir $(wildcard $(ARDUINO_PLATFORM_LIB_PATH)/*)), \
         $(shell sed -ne 's/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p' $(LOCAL_SRCS)))
+<<<<<<< HEAD
 =======
         $(shell sed -ne "s/^ *\# *include *[<\"]\(.*\)\.h[>\"]/\1/p" $(LOCAL_SRCS)))
     ARDUINO_LIBS += $(MAIN_LIBS) $(LIBS_DEPS)
 >>>>>>> origin/auto-lib
+=======
+>>>>>>> headers
 endif
 
 ########################################################################
@@ -1125,6 +1138,7 @@ else
     $(call show_config_info,Size utility: Basic (not AVR-aware),[AUTODETECTED])
 endif
 
+<<<<<<< HEAD
 ifneq (,$(strip $(MAIN_LIBS)))
     $(call arduino_output,-)
     $(call show_config_info,MAIN_LIBS =)
@@ -1146,6 +1160,15 @@ endif
 ifneq (,$(strip $(SYS_LIBS)))
     $(call arduino_output,-)
     $(call show_config_info,SYS_LIBS =)
+=======
+ifneq (,$(strip $(ARDUINO_LIBS)))
+    $(call arduino_output,-)
+    $(call show_config_info,ARDUINO_LIBS =)
+endif
+
+ifneq (,$(strip $(USER_LIB_NAMES)))
+    $(foreach lib,$(USER_LIB_NAMES),$(call show_config_info,  $(lib),[USER]))
+>>>>>>> headers
 endif
 
 ifneq (,$(strip $(SYS_LIB_NAMES)))
